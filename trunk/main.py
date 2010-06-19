e95d47456b1bc1fdafcb3611a6f1964a7a082d39
guilbep <guilbep@fa385e5f-d618-6b73-4d46-620999c69b8f>
2010-06-19 15:48:38 +0000

    fuzzification in progress

--- trunk/main.py
+++ trunk/main.py
@@ -1,16 +1,114 @@
 from neuron import *
-from node import *
 from arc import *
 
-a = 3
-b = 4
+poudre = 2
 
-test_primary_node = neuron(node(None,None), a) # second parameter is fct => 1
-test_secondary_node = neuron(node(None,None), b)
+###### entree
 
-arc1 = arc(1, test_primary_node.node, test_secondary_node.node)
+# entree d'erreur
+err = 0
+# entree de delta erreur
+derr = 0
+
+# entree de maxErr
+maxError = 10
+# entree de maxDeltaEr
+maxDerr = 10
+# entree de maxDeltaPoudre
+maxDpoudre = 10
 
 
-print test_primary_node.function
-print test_secondary_node.function
+### neuronne
+def fune(x):
+	return err
+def funde(x):
+	return derr
+def funMaxE(x):
+	return maxError
+def funMaxDE(x):
+	return maxDpoudre
+def funMaxDP(x):
+	return maxDpoudre
 
+neuronErreur = neuron(fune)
+neuronDErreur = neuron(funde)
+neuronMaxE = neuron(funMaxE)
+neuronMaxDE = neuron(funMaxDE)
+neuronMaxDP = neuron(funMaxDP)
+
+###### fin entree // debut de boucle?
+
+###### fuzzification
+
+# learn how to draw gaussienne
+()*exp((-0.5)*((x - mu)/(sigma))^2)
+def gaussienne(x,mu,sigma):
+	return ()
+
+## on a six regles 3 pour le delta erreur et 3 pour le erreur
+## neuron erreur
+neuronErrorLessThan = neuron()
+neuronErrorEqual
+neuronErrorMoreThan
+
+## neuron delta erreur
+neuronDErrorLessThan
+neuronDErrorEqual
+neuronDErrorMoreThan
+
+## binding des entrees avec la fuzzification
+
+###### end of fuzzification
+
+###### SUGENO
+
+
+###### end of SUGENO
+
+###### Defuzzification
+
+
+###### end of Defuzzification
+
+
+### multiplication exemple
+def funa(x):
+	return 4
+
+def funb(x):
+	return 3
+
+def funcarre(x):
+	return x*x
+	
+def fununit(x):
+	return x
+
+neuronA = neuron(funa)
+neuronB = neuron(funb)
+
+neuronAplusBcarre = neuron(funcarre)
+neuronAcarre = neuron(funcarre)
+neuronBcarre = neuron(funcarre)
+
+neuronA.bindTo(neuronAplusBcarre,1)
+neuronB.bindTo(neuronAplusBcarre,1)
+
+neuronA.bindTo(neuronAcarre,1)
+neuronB.bindTo(neuronBcarre,1)
+
+neuronMult = neuron(fununit)
+
+
+neuronAplusBcarre.bindTo(neuronMult, 0.5)
+neuronAcarre.bindTo(neuronMult, -0.5)
+neuronBcarre.bindTo(neuronMult, -0.5)
+
+#print "neuronA", (neuronA.function)
+#print "neuronB", (neuronB.function)
+#print "neuronAplusBcarre",(neuronAplusBcarre.function)
+#print "neuronAcarre",(neuronAcarre.function)
+#print "neuronBcarre",(neuronBcarre.function)
+print "NeuronMult",(neuronMult.function)
+
+### fin multiplication exemple
--- trunk/neuron.py
+++ trunk/neuron.py
@@ -1,21 +1,43 @@
+from arc import *
+
 class neuron(object):
-	def __init__(self, node, function):
-		self._node = node
+	def __init__(self, function):
 		self._function = function
+		self._inArcs = []
+		self._outArcs = []
+
+	def getInArcs(self):
+		return self._inArcs
+	def setInArcs(self, value):
+		self._inArcs = value
+	def delInArcs(self):
+		del self._inArcs
+
+	inArcs = property(getInArcs, setInArcs, delInArcs, "I'm the 'InArcs' property.")
+
+	def getOutArcs(self):
+		return self._outArcs
+	def setOutArcs(self, value):
+		self._outArcs = value
+	def delOutArcs(self):
+		del self._outArcs
+	outArcs = property(getOutArcs, setOutArcs, delOutArcs, "I'm the 'OutArcs' property.")
+
 
 	def getf(self):
-		return self._function
+		sumNodeWeight = 0
+		for i in range(len(self._inArcs)):
+		   sumNodeWeight += self._inArcs[i].weight * self._inArcs[i].beginNeurone.function
+		return self._function(sumNodeWeight)
 	def setf(self, value):
 		self._function = value
 	def delf(self):
 		del self._function
 	function = property(getf, setf, delf, "I'm the 'node' property.")
+	
+	
+	def bindTo(self, neurone, weight):
+		tempArc = arc(weight, self, neurone)
+		neurone.inArcs += [tempArc]
+		self._outArcs += [tempArc]
 
-
-	def getn(self):
-		return self._node
-	def setn(self, value):
-		self._node = value
-	def deln(self):
-		del self._node
-	node = property(getn, setn, deln, "I'm the 'node' property.")
