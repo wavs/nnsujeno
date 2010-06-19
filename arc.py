c9c287868939d23701388ddcabbf1aa24e16bc6e
guilbep <guilbep@fa385e5f-d618-6b73-4d46-620999c69b8f>
2010-06-19 15:48:38 +0000

    fuzzification in progress

--- arc.py
+++ arc.py
@@ -1,32 +1,29 @@
 class arc(object):
-	def __init__(self, weight, beginNode, endNode):
+	def __init__(self, weight, beginNeurone, endNeurone):
 		self._weight = weight
-		## add arc to BeginNode
-		self._beginNode = beginNode
-		self._endNode = endNode
+		self._beginNeurone = beginNeurone
+		self._endNeurone = endNeurone
 	
 	def getweight(self):
-		return self.weight
+		return self._weight
 	def setweight(self, value):
 		self._weight = value
 	def delweight(self):
 		del self._weight
 	weight = property(getweight, setweight, delweight, "I'm the 'weight' property.")
 	
-	def getbeginNode(self):
-		return self._beginNode
-	def setbeginNode(self, value):
-		## delete arc in current beginnode
-		self._beginNode = value
-	def delbeginNode(self):
-		del self._beginNode
-	beginNode = property(getbeginNode, setbeginNode, delbeginNode, "I'm the 'beginNode' property.")
+	def getbeginNeurone(self):
+		return self._beginNeurone
+	def setbeginNeurone(self, value):
+		self._beginNeurone = value
+	def delbeginNeurone(self):
+		del self._beginNeurone
+	beginNeurone = property(getbeginNeurone, setbeginNeurone, delbeginNeurone, "I'm the 'beginNeurone' property.")
 	
-	def getendNode(self):
-		return self._endNode
-	def setendNode(self, value):
-		## delete link in current endnode
-		self._endNode = value
-	def delendNode(self):
-		del self._endNode
-	endNode = property(getendNode, setendNode, delendNode, "I'm the 'endNode' property.")
+	def getendNeurone(self):
+		return self._endNeurone
+	def setendNeurone(self, value):
+		self._endNeurone = value
+	def delendNeurone(self):
+		del self._endNeurone
+	endNeurone = property(getendNeurone, setendNeurone, delendNeurone, "I'm the 'endNeurone' property.")
