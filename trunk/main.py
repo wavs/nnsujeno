fae72ae4f861311410674cf52d1cd9bff6ff1e71
guilbep <guilbep@fa385e5f-d618-6b73-4d46-620999c69b8f>
2010-06-20 14:54:56 +0000

    Fuzzyification done; todo sugeno and defuzzification

--- trunk/main.py
+++ trunk/main.py
@@ -102,30 +102,6 @@ neuronMaxDP.bindTo(neuronmuDPmaxMT, maxDpoudre)
 neuronmuDPmaxMMT = neuron(funtwothird)
 neuronMaxDP.bindTo(neuronmuDPmaxMMT, maxDpoudre)
 
-def gaussienne(x,mu,sigma):
-	return (1/(sigma*sqrt(2*pi))) * exp( -0.5 * ( pow((x - mu) / (sigma),2 ) ))	
-def gaussEqual(x, error): # equal error
-	return gaussienne(x,0,0.166666667*error) 
-def gaussLT(x, error): # less than error
-	return gaussienne(x, -0.5*error,0.166666667*error)
-def gaussMT(x, error): #more than error
-	return gaussienne(x, 0.5*error,0.166666667*error)
-
-
-
-## on a six regles 3 pour le delta erreur et 3 pour le erreur
-## neuron erreur
-#euronErrorLessThan = neuron()
-#neuronErrorEqual = neuron()
-#neuronErrorMoreThan = neuron()
-
-## neuron delta erreur
-#neuronDErrorLessThan = neuron()
-#neuronDErrorEqual = neuron()
-#neuronDErrorMoreThan = neuron()
-
-## binding des entrees avec la fuzzification
-
 ###### end of fuzzification
 
 ###### SUGENO
