dea3912831574d3b2cbcc9e62fb0c7a7f01fab9e
thomas.aittaleb <thomas.aittaleb@fa385e5f-d618-6b73-4d46-620999c69b8f>
2010-06-21 15:06:28 +0000

    Added test file.

---
+++ testGraphMatPlotLib.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pylab as pl
+from scipy.special import jn
+from math import *
+from physic_cmd import *
+
+times = []
+valuesOfError = []
+valuesOfDeltaError = []
+time = 0
+
+""" Variables """
+consigne = 50
+error = 0.5
+derror = 0
+
+physic = physicSystem(consigne)
+
+while (error > 0.1):
+	
+	error = physic.getNewError()
+	derror = physic.getNewDeltaError(error)
+	physic.computeNewPosition(error)
+	print error, ":", derror	
+	times.append(time)
+	time = time + 1
+	valuesOfError.append(error)
+	valuesOfDeltaError.append(derror)
+
+pl.plot(times, valuesOfError, label='Error')
+pl.plot(times, valuesOfDeltaError, label='DeltaError')
+pl.title('Evolution de lerreur et de sa derivee')
+pl.legend()
+
+pl.show()
